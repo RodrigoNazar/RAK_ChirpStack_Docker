d8a5698d944bbc93f6d8a09174502bd6a465f0e6
Rodrigo Nazar Meier <33737276+RodrigoNazar@users.noreply.github.com>
2023-05-05 15:59:24 -0400

    DockerSetup at setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Install utility commands
+
+apt update
+apt install sudo
+
+sudo adduser chirpstack
+su - chirpstack
+
+
+sudo apt install vim
+sudo apt install git
+
+echo 'PATH=$PATH:/usr/lib/postgresql/14/bin'; >> ~/.bashrc
