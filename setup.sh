b51156fed82076919db91b6a3baf9e5903958868
Rodrigo Nazar Meier <rnazar@uc.cl>
2023-05-08 16:27:48 -0400

    Automated push by Gitt

--- setup.sh
+++ setup.sh
@@ -5,5 +5,3 @@ echo 'PATH=$PATH:/usr/lib/postgresql/14/bin' >> ~/.bashrc
 echo 'alias chirpstack-network-server=/etc/init.d/chirpstack-network-server' >> ~/.bashrc
 echo 'alias chirpstack-application-server=/etc/init.d/chirpstack-application-server' >> ~/.bashrc
 echo 'alias chirpstack-gateway-bridge=/etc/init.d/chirpstack-gateway-bridge' >> ~/.bashrc
-
-source ~/.bashrc
